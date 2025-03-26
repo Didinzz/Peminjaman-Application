4c035912f04e29128e10bff9a6f30065676f4d8c
Didinzz <didinzakaria123@gmail.com>
2025-03-26 13:16:44 +0800

    change button pengajuan

--- app/Filament/Resources/PeminjamanResource/Pages/ListPeminjamen.php
+++ app/Filament/Resources/PeminjamanResource/Pages/ListPeminjamen.php
@@ -13,7 +13,8 @@ class ListPeminjamen extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+            ->label('Ajukan Peminjaman'),
         ];
     }
 }
